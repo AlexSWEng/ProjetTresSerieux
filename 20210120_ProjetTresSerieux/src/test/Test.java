e32f36dbd4e47d24a453029489e890df4d34488c
Alex <72945711+AlexSWEng@users.noreply.github.com>
2021-01-20 19:28:13 +0100

    Update Test.java

--- 20210120_ProjetTresSerieux/src/test/Test.java
+++ 20210120_ProjetTresSerieux/src/test/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public static void main( String[] a ) {
         System.out.println( "mechant !" );
         System.out.println( "yohoho" );
+        System.out.println( "yohoho33333" );
     }
 }

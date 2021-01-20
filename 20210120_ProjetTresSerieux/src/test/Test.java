a30c9055104b3d720715bed7a4ef30cce6ef8de3
Alex <72945711+AlexSWEng@users.noreply.github.com>
2021-01-20 19:56:57 +0100

    Update Test.java

--- 20210120_ProjetTresSerieux/src/test/Test.java
+++ 20210120_ProjetTresSerieux/src/test/Test.java
@@ -2,8 +2,7 @@ package test;
 
 public class Test {
 
-    public static void main( String[] a ) {
-        System.out.println( "mechant !" );
-        System.out.println( "yohoho" );
+    public static void main( String[] args ) {
+        System.out.println( "1" );
     }
 }

6331c1c79abee99daaedd2ad4b6964ae724aabf8
Alex <72945711+AlexSWEng@users.noreply.github.com>
2021-01-20 19:53:27 +0100

    Update Test.java
    pas de sysout pls

--- 20210120_ProjetTresSerieux/src/test/Test.java
+++ 20210120_ProjetTresSerieux/src/test/Test.java
@@ -5,11 +5,5 @@ public class Test {
     public static void main( String[] a ) {
         System.out.println( "mechant !" );
         System.out.println( "yohoho" );
-        System.out.println( "3" );
-        System.out.println( "4" );
-        System.out.println( "5" );
-        System.out.println( "6" );
-        System.out.println( "7" );
-
     }
 }

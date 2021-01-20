0994e0916d75999d0282630dd30937949fe7f85c
Alex <aer2097@gmail.com>
2021-01-20 19:32:05 +0100

    test

--- 20210120_ProjetTresSerieux/src/test/Test.java
+++ 20210120_ProjetTresSerieux/src/test/Test.java
@@ -5,10 +5,6 @@ public class Test {
     public static void main( String[] a ) {
         System.out.println( "mechant !" );
         System.out.println( "yohoho" );
-<<<<<<< HEAD
-        System.out.println( "yohoho222" );
-=======
-        System.out.println( "yohoho33333" );
->>>>>>> branch 'ALEX_dev' of https://github.com/AlexSWEng/ProjetTresSerieux
+
     }
 }
